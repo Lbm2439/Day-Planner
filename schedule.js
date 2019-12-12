bc17577fe621a0425c6671cbbc0e92114812a245
Rapunzel <Laurenminton19@yahoo.com>
2019-12-11 19:59:20 -0500

    added save buttons, forms, and some javascript

---
+++ schedule.js
@@ -0,0 +1,19 @@
+//save button
+
+function saveButton(){
+	if (){
+		
+	}
+	else {
+		alert("You must enter a to-do.")
+	}
+}
+
+//color changes for past-present-future
+// else if ??
+
+if (condition) {
+    
+} else if {
+    
+}
